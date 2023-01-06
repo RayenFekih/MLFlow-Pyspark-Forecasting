d1fc30a3a2f1bf4f83ec644809ea8a6a4ad12e08
Mohamed Rayen Fekih <mohamedrayen.fekih@cognira.com>
2023-01-06 10:00:56 +0100

    adding lightGBM model

--- modelling/models.py
+++ modelling/models.py
@@ -1,8 +1,8 @@
 from abc import ABC, abstractmethod
 
 import xgboost as xgb
+import lightgbm as lgbm
 import mlflow
-from mlflow import sklearn
 
 
 class Model(ABC):
@@ -37,8 +37,34 @@ class XGBoostModel(Model):
     def mlflow_logs(self):
         mlflow.xgboost.autolog()
 
-    def mlflow_log_model(self):
-        sklearn.log_model(self, "model")
+    def mlflow_log_model(self, flavor="sklearn"):
+        if flavor == "sklearn":
+            mlflow.sklearn.log_model(self, "model")
+        # [TODO]: Add more mlflow flavors
+        # if flavor == "xgboost":
+        #     mlflow.xgboost.log_model(self, "model")
+
+    def fit(self, X_train, y_train):
+        return self.model.fit(X_train, y_train)
+
+    def predict(self, X_test):
+        return self.model.predict(X_test)
+
+class LgbmModel(Model):
+    def __init__(self, params: dict = {}) -> None:
+        self.params = params
+        self.model = lgbm.LGBMRegressor(**params)
+
+    @classmethod
+    def new_instance(cls, params={}):
+        return cls(params)
+
+    def mlflow_logs(self):
+        mlflow.lightgbm.autolog()
+    
+    def mlflow_log_model(self, flavor="sklearn"):
+        if flavor == "sklearn":
+            mlflow.sklearn.log_model(self, "model")
 
     def fit(self, X_train, y_train):
         return self.model.fit(X_train, y_train)
